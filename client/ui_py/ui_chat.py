4b007b9553d92be3594a2962466d1bc5e23f1584
Zrp <1346650303@qq.com>
2023-04-15 23:08:01 +0800

    Revert "文件传server"
    This reverts commit 42f68ba6332a948f8f402072fb2d08e48b360cb1.

--- client/ui_py/ui_chat.py
+++ client/ui_py/ui_chat.py
@@ -6,7 +6,9 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QTextEdit
+
 
 class Ui_chatWindow(object):
     def setupUi(self, chatWindow):
@@ -16,17 +18,12 @@ class Ui_chatWindow(object):
         self.verticalLayout.setObjectName("verticalLayout")
         self.label_channelName = QtWidgets.QLabel(chatWindow)
         self.label_channelName.setStyleSheet("QLabel{\n"
-"background-color:rgb(255, 170, 0);\n"
-"}")
+                                             "background-color:rgb(255, 170, 0);\n"
+                                             "}")
         self.label_channelName.setTextFormat(QtCore.Qt.RichText)
         self.label_channelName.setAlignment(QtCore.Qt.AlignCenter)
         self.label_channelName.setObjectName("label_channelName")
         self.verticalLayout.addWidget(self.label_channelName)
-        self.line_2 = QtWidgets.QFrame(chatWindow)
-        self.line_2.setFrameShape(QtWidgets.QFrame.HLine)
-        self.line_2.setFrameShadow(QtWidgets.QFrame.Sunken)
-        self.line_2.setObjectName("line_2")
-        self.verticalLayout.addWidget(self.line_2)
         self.textBrowser_show = QtWidgets.QTextBrowser(chatWindow)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
@@ -35,21 +32,16 @@ class Ui_chatWindow(object):
         self.textBrowser_show.setSizePolicy(sizePolicy)
         self.textBrowser_show.setObjectName("textBrowser_show")
         self.verticalLayout.addWidget(self.textBrowser_show)
-        self.line = QtWidgets.QFrame(chatWindow)
-        self.line.setFrameShape(QtWidgets.QFrame.HLine)
-        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
-        self.line.setObjectName("line")
-        self.verticalLayout.addWidget(self.line)
-        self.textEdit_input = QtWidgets.QTextEdit(chatWindow)
+        self.textEdit_input = QTextEdit(chatWindow)
         self.textEdit_input.setObjectName("textEdit_input")
         self.verticalLayout.addWidget(self.textEdit_input)
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
         spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem)
-        self.pushButton_sendFile = QtWidgets.QPushButton(chatWindow)
-        self.pushButton_sendFile.setObjectName("pushButton_sendFile")
-        self.horizontalLayout.addWidget(self.pushButton_sendFile)
+        self.pushButton_sendImg = QtWidgets.QPushButton(chatWindow)
+        self.pushButton_sendImg.setObjectName("pushButton_sendImg")
+        self.horizontalLayout.addWidget(self.pushButton_sendImg)
         spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem1)
         self.pushButton_send = QtWidgets.QPushButton(chatWindow)
@@ -71,6 +63,5 @@ class Ui_chatWindow(object):
         _translate = QtCore.QCoreApplication.translate
         chatWindow.setWindowTitle(_translate("chatWindow", "Form"))
         self.label_channelName.setText(_translate("chatWindow", "TextLabel"))
-        self.pushButton_sendFile.setText(_translate("chatWindow", "选择文件"))
+        self.pushButton_sendImg.setText(_translate("chatWindow", "选择图片"))
         self.pushButton_send.setText(_translate("chatWindow", "发送"))
-
--- client/utils/myWidgets.py
+++ client/utils/myWidgets.py
@@ -1,62 +1,41 @@
-from PyQt5 import QtCore
-from PyQt5.QtCore import QFileInfo, QUrl, QFile, QIODevice, QByteArray, \
-    QBuffer
-from PyQt5.QtGui import QImage, QImageReader, QTextDocumentFragment
-from PyQt5.QtWidgets import QTextEdit
+import base64
 
-from datetime import datetime
+from PyQt5.QtWidgets import QApplication, QTextEdit
+from PyQt5.QtGui import QImage, QPixmap, QDragEnterEvent, QDropEvent, QMouseEvent, QKeyEvent
+from PyQt5.QtCore import Qt, QMimeData, QByteArray, QBuffer
 
 
-class TextEdit(QTextEdit):
+class MyTextEdit(QTextEdit):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.setAcceptDrops(True)
 
-    def canInsertFromMimeData(self, source: QtCore.QMimeData) -> bool:
-        return source.hasImage() or source.hasUrls() or \
-            super(TextEdit, self).canInsertFromMimeData(source)
+    def dragEnterEvent(self, event: QDragEnterEvent):
+        if event.mimeData().hasImage():
+            event.acceptProposedAction()
 
-    def insertFromMimeData(self, source: QtCore.QMimeData) -> None:
+    def dragMoveEvent(self, event: QDragEnterEvent):
+        if event.mimeData().hasImage():
+            event.acceptProposedAction()
+
+    def dropEvent(self, event: QDropEvent):
+        if event.mimeData().hasImage():
+            image = QImage(event.mimeData().imageData())
+            self.insertImage(image)
+            event.acceptProposedAction()
+
+    def insertFromMimeData(self, source: QMimeData):
         if source.hasImage():
-            self.insert_image(source.imageData())
-        elif source.hasUrls():
-            for url in source.urls():
-                file_info = QFileInfo(url.toLocalFile())
-                ext = file_info.suffix().lower()
-                if ext in QImageReader.supportedImageFormats():
-                    self.insert_image(QImage(file_info.filePath()), ext)
-                else:
-                    self.insert_file(url)
+            image = QImage(source.imageData())
+            self.insertImage(image)
         else:
-            super(TextEdit, self).insertFromMimeData(source)
+            super().insertFromMimeData(source)
 
-    def insert_image(self, image: QImage, fmt: str = "png"):
-        """插入图片"""
+    def insertImage(self, image: QImage):
+        cursor = self.textCursor()
         data = QByteArray()
         buffer = QBuffer(data)
-        image.save(buffer, fmt)
-        base64_data = str(data.toBase64())[2:-1]
-        data = f'<img src="data:image/{fmt};base64,{base64_data}" />'
-        fragment = QTextDocumentFragment.fromHtml(data)
-        self.textCursor().insertFragment(fragment)
-
-    def insert_file(self, url: QUrl):
-        """插入文件"""
-        file = None
-        # noinspection PyBroadException
-        try:
-            file = QFile(url.toLocalFile())
-            if not file.open(QIODevice.ReadOnly or QIODevice.Text):
-                return
-            file_data = file.readAll()
-            # noinspection PyBroadException
-            try:
-                self.textCursor().insertHtml(str(file_data, encoding="utf8"))
-            except Exception:
-                self.textCursor().insertHtml(str(file_data))
-        except Exception:
-            if file:
-                file.close()
-
-    @staticmethod
-    def get_time_str():
-        return datetime.now().strftime("%Y%#m%#d_%H%M%S")
+        image.save(buffer)
+        base = str(data.toBase64())
+        print(base)
+        cursor.insertHtml('<img src="data:image/png;base64,{0}"/>'.format(base))
